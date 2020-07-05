a884f80127b5d3e8ddede30dd77e0a1d5d2538bf
xiejianzhang <xiejianzhangmail@163.com>
2020-07-05 20:21:54 +0800

    contract—instance

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {CardContent} from "semantic-ui-react";
 
 let web3 = require('./utils/initWeb3')
 let lotteryInstance = require('./eth/lotteryInstance')
@@ -12,13 +13,13 @@ class App extends Component {
     constructor() {
         super();
 
-        this.state ={
-            manager : '',
-            round:'',
-            winner:'',
-            palyerCount:0,
-            balance:0,
-            palyers:[],
+        this.state = {
+            manager: '',
+            round: '',
+            winner: '',
+            palyerCount: 0,
+            balance: 0,
+            palyers: [],
 
         }
     }
@@ -55,14 +56,18 @@ class App extends Component {
         let a = 'HelloWorld'
         return (
             <div>
-                <p>helloworld:{a}</p>
-                <p>manger:{this.state.manager}</p>
-                <p>round:{this.state.round}</p>
-                <p>winner:{this.state.winner}</p>
-                <p>balance:{this.state.balance}</p>
-                <p>palyers:{this.state.palyers}</p>
-                <p>palyerCounts:{this.state.palyerCounts}</p>
 
+                <CardContent
+                    helloworld:{a}
+                    manger:{this.state.manager}
+                    round:{this.state.round}
+                    winner:{this.state.winner}
+                    balance:{this.state.balance}
+                    palyers:{this.state.palyers}
+                    palyerCounts:{this.state.palyerCounts}
+                    play={this.play()} //在ui.js button 里面添加函数
+
+                />
             </div>
         );
     }
